70495e00daab25320009620ef400ce1f3a27c69c
chuokoh <chukwuebukajosh8@gmail.com>
2026-05-14 16:48:57 +0100

    feat: parseflags() function for getting and validating the arguments coloredindices() function to get the positions of the substrings in the sentence

--- main.go
+++ main.go
@@ -3,29 +3,65 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	parseArgs := parseFlags(os.Args)
+	color := parseArgs.color
+	subStr := parseArgs.subStr
+	sentence := parseArgs.sentence
 
-	flag := os.Args[1]
+	fmt.Println(color, subStr, sentence)
+	fmt.Println(coloredIndices("a king kitten have kit", "kit"))
+}
 
-	switch len(os.Args) {
-	case 2:
-	case 3:
-		color, ok := checkValidate(flag)
-		if !ok {
-			fmt.Println("Usage: go run . [OPTION] [STRING]")
-			return
+type FlagConfig struct {
+	color    string
+	subStr   string
+	sentence string
+}
+
+func parseFlags(s []string) FlagConfig {
+	switch {
+	case len(s) == 2:
+		return FlagConfig{
+			sentence: s[1],
 		}
-	case 4:
-		color, ok := checkValidate(flag)
-		if !ok {
-			fmt.Println("Usage: go run . [OPTION] [STRING]")
-			return
+	case len(s) == 4:
+		arg := s[1]
+		if strings.HasPrefix(arg, "--color=") {
+			split := strings.Split(arg, "=")
+			return FlagConfig{
+				color:    split[1],
+				subStr:   s[2],
+				sentence: s[3],
+			}
 		}
-
+		fmt.Println("Usage: go run . [OPTION] [STRING]\nEX: go run . --color=<color> <substring to be colored> \"something\"")
+		os.Exit(1)
 	default:
-		fmt.Println("Error: not enough arguement")
-		return
+		fmt.Println("Usage: go run . [OPTION] [STRING]\nEX: go run . --color=<color> <substring to be colored> \"something\"")
+		os.Exit(1)
 	}
+	return FlagConfig{}
+}
+
+func coloredIndices(sentence, subStr string) map[int]bool {
+	found := make(map[int]bool)
+	remaining := sentence
+	offset := 0
+
+	for {
+		pos := strings.Index(remaining, subStr)
+		if pos == -1 {
+			break
+		}
+		for i := 0; i < len(subStr); i++ {
+			found[pos+offset+i] = true
+		}
+		offset += pos + len(subStr)
+		remaining = remaining[pos+len(subStr):]
+	}
+	return found
 }
